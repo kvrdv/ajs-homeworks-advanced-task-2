7fa2745469d8e7e5768c38f7cd37a1d64678cec7
Semyon Kovardaev <s.kovardaev@gmail.com>
2020-08-16 22:31:05 +0300

    second commit

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,40 @@
+import showSpells from '../app';
+
+test('Вывод способностей 1', () => {
+  const received = showSpells({
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      },
+    ],
+  });
+  const expected = [
+    {
+      description: 'Двойной выстрел наносит двойной урон',
+      icon: 'http://...',
+      id: 8,
+      name: 'Двойной выстрел',
+    },
+    {
+      description: 'Не указан',
+      icon: 'http://...',
+      id: 9,
+      name: 'Нокаутирующий удар',
+    },
+  ];
+  expect(received).toEqual(expected);
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,15 @@
+export default function showSpells(character) {
+  const {
+    name, type, health, level, attack, defence, ...rest
+  } = character;
 
+  const spells = rest.special;
+
+  for (let i = 0; i < spells.length; i += 1) {
+    if (spells[i].description === undefined) {
+      spells[i].description = 'Не указан';
+    }
+  }
+
+  return spells;
+}
