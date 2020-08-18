47851042acae56c1c65b62b3cc15640606d9718b
Semyon Kovardaev <s.kovardaev@gmail.com>
2020-08-18 17:45:04 +0300

    second commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,10 @@
 export default function showSpells(character) {
-  const {
-    name, type, health, level, attack, defence, ...rest
-  } = character;
+  const spells = [];
+  const { name, type, health, level, attack, defence, ...rest } = character;
 
-  const spells = rest.special;
-
-  for (let i = 0; i < spells.length; i += 1) {
-    if (spells[i].description === undefined) {
-      spells[i].description = 'Не указан';
-    }
+  for (let i = 0; i < rest.special.length; i += 1) {
+    const { icon, id, name, description = 'Описание недоступно', } = rest.special[i];
+    spells.push(rest.special[i])
   }
 
   return spells;
